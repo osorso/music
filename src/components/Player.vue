7844709439bcbb03a9f2343e7292c3b46832952d
唯影 <807781817@qq.com>
2020-01-16 15:53:27 +0800

    encapsulation request and add eslintrc.js

---
+++ src/components/Player.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="player-box">
+    <div class="player">
+      <audio src=""></audio>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  props: {
+    musicInfo: {
+      type: Object,
+      required: true,
+      default() {
+        return {
+          src: ''
+        }
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
